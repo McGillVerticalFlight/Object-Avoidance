fc0d3f0983a945a554fddb0062c1595f92cf65fe
Yijun Zhang <yijun.zhang@mail.mcgill.ca>
2023-01-09 22:13:47 -0500

    Outline ver1

---
+++ CameraToData.py
@@ -0,0 +1,11 @@
+
+#This is different from a typical COMP class asignment
+#Feel free to set many arguments and helper functions
+#no tester nor edge cases. Find it during coding
+def getDepth():
+    '''
+    Get a 2D matrix containing all depth information for one frame
+    Note: time complexity matters. Space complexity may matter (don't know yet).Try to minimize the cost.
+    :return: one 2D array
+    '''
+    return None
