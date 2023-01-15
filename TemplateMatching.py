d536f21805c6b131a9ca5020aaaf0c8edc5bb120
Yijun Zhang <yijun.zhang@mail.mcgill.ca>
2023-01-14 23:36:05 -0500

    Outline ver3 with TemplateMatching.py

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -4,6 +4,8 @@ import numpy as np
 def templateMatch():
     '''
     finding the landing pad in a frame
+    knowing the depth from camera to ground/object
+    will make this method better with a resizing method
 
     :return: boolean
     '''
@@ -21,6 +23,7 @@ def templateMatch():
 
         #template matching
         template = cv2.imread('testingTemplate.jpg')
+        template = resizing(template,1,1) #not resizing right now
         h, w= template.shape[0:2]
 
         # all template matching methods in openCV library: [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED
@@ -31,18 +34,22 @@ def templateMatch():
         img2 = frame.copy()
 
         #display matches
-        method =cv2.TM_CCORR
+        method =cv2.TM_CCOEFF_NORMED
         result = cv2.matchTemplate(img2,template,method)
+
         min_val,max_val,min_loc,max_loc = cv2.minMaxLoc(result)
         if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
             top_left = min_loc
         else:
             top_left = max_loc
 
-        #draw rectangle around matches
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-        cv2.rectangle(img2,top_left,bottom_right,255,5)
-        cv2.imshow('Match',img2)
+
+        if((method in [cv2.TM_CCOEFF_NORMED,cv2.TM_CCORR_NORMED,cv2.TM_SQDIFF_NORMED]) and (max_val >= 0.5)):
+            #draw rectangle around the match
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            cv2.rectangle(img2,top_left,bottom_right,255,5)
+
+        cv2.imshow('Match', img2)
 
         #wait 1 milsec
         #if key q is pressed, quit the whole process
@@ -55,7 +62,26 @@ def templateMatch():
 
     return False
 
-def centerOfFrame():
+def resizing(template,focal,depth):
+    '''
 
+    formula: size in camera = actual size * focal/depth
+
+    :param filename: template filename
+    :param height: actual object height
+    :param width: actual object width
+    :param focal: focal length
+    :param depth: distance between camera to object/ground
+    :return: resized template
+    '''
+    ratio = focal/depth
+    img = cv2.resize(template,(0,0),fx=ratio,fy=ratio)
+    return img
+
+def centerOfFrame():
+    '''
+    identify the center of the object to track
+    :return: pixel coordinate maybe?
+    '''
     return None
 templateMatch()
